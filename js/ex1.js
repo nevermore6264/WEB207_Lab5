269f357d7af0bfb2aa8de3baff1cc56792b901fa
Hieutt <nevermore6264@gmail.com>
2019-04-01 18:31:14 +0700

    convert js file

--- js/ex1.js
+++ js/ex1.js
@@ -1,16 +1,28 @@
 var app = angular.module("myApp", []);
 app.controller("add", function ($scope) {
+    $('.input-group.date').datepicker({format: "dd.mm.yyyy"});
     $scope.save = function () {
         $scope.$parent.students[$scope.$parent.index] = $scope.student;
     }
     $scope.cancel = function () {
         $scope.$parent.student = angular.copy($scope.$parent.students[$scope.$parent.index]);
     }
+    $(function() {
+        $( "#datepicker" ).datepicker({
+            dateFormat : 'dd-mm-yy',
+            changeMonth : true,
+            changeYear : true,
+            yearRange: '-100y:c+nn',
+            maxDate: '-1d',
+            onSelect: function(date) {
+                angular.element($('#datepicker')).triggerHandler('input');
+            }
+        });
+    });
+
+
 });
 app.controller("show", function ($scope) {
-    $scope.model = {
-        isFormDisabled : false
-    }
 
     $scope.$parent.students = [
         {
@@ -25,7 +37,6 @@ app.controller("show", function ($scope) {
         }
     ];
     $scope.edit = function (index) {
-        isFormDisabled = true;
         $scope.$parent.index = index;
         $scope.$parent.student = angular.copy($scope.$parent.students[index]);
     }
